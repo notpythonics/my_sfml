db48a9b27074da2cecf820ea471067b04b3b0ac6
moamen <moamenalali748117@hotmail.com>
2024-06-03 20:36:38 +0300

    anti alising and contexts

--- SFML/main.cpp
+++ SFML/main.cpp
@@ -5,7 +5,10 @@ constexpr float WIDTH = 800;
 constexpr float HEIGHT = 600;
 
 int main(){
-	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "game");
+	sf::ContextSettings context; // a collection of varibales
+	context.antialiasingLevel = 8;
+
+	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "game", sf::Style::Default, context);
 	sf::CircleShape shape(20.0f);
 	shape.setFillColor(sf::Color::Magenta);
 	shape.setPosition(100.0f, 100.0f);
@@ -16,6 +19,17 @@ int main(){
 	rectangle.setPosition(sf::Vector2f(400, 400));
 	rectangle.setRotation(90);
 	
+
+	sf::Texture player_texture;
+	sf::Sprite player_sprite;
+	player_sprite.setScale(sf::Vector2f(0.2, 0.2));
+
+	if (!player_texture.loadFromFile("assests\\player.png")) {
+		std::cout << 'n';
+		return 1;
+	}
+	player_sprite.setTexture(player_texture);
+
 	//game loop
 	while (window.isOpen()) {
 		sf::Event event;
@@ -24,11 +38,17 @@ int main(){
 			if (event.type == sf::Event::Closed) {
 				window.close();
 			}
+
+			if (event.type == sf::Event::KeyPressed) {
+				if(event.key.code == sf::Keyboard::D)
+					player_sprite.setPosition(player_sprite.getPosition() + sf::Vector2f(50,0));
+			}
 		}
 		// clearing without displaying throws an exception
 		window.clear(sf::Color::Black); // clear bellon al asoad     it's on the back buffer
 		
 		window.draw(shape);
+		window.draw(player_sprite);
 		window.draw(rectangle);
 
 		window.display(); // copies the data from the back buffer to the screen
